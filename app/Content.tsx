d74de66e79deef2137aab3d2c0b4f89042a42971
Niortien <Niortiencoulibaly2001@gmail.com>
2025-02-24 13:58:35 +0000

    first

---
+++ app/Content.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Content = () => {
+    return (
+        <div className='h-96 bg-white-300'>
+            kk
+        </div>
+    );
+}
+
+export default Content;
---
+++ app/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='h-96 bg-orange-600'>
+            <h1>kk</h1>
+        </div>
+    );
+}
+
+export default Footer;
