28392377d0899e0b4181250eb1a67964d1402d7d
Anelia <aneliatahibet02@gmail.com>
2025-03-17 12:11:33 +0100

    new

--- app/collection/page.tsx
+++ app/collection/page.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
-import Navbar from '@/components/common/Navbar';
-import Watches from '@/app/collection/Watches';
-import SortList from './SortList';
+import React from "react";
+import SortList from "@/components/collection/SortList";
+import Watches from "@/components/collection/Watches";
 
-export default function Collection() {
+import { Watch } from "lucide-react";
+import ProductList from "@/components/collection/ProductList";
+export default async function Collection() {
   return (
-    <div>
-          <Navbar/>
-      <div className='flex sm:gap-10'> <SortList/> <Watches/></div>
-    </div>
-  )
+    <>
+      <div className="">
+        <Watches />
+        <div className="flex gap-10 mx-30">
+          <SortList />
+          <div>
+         <ProductList/>
+          </div>
+         
+          
+        </div>
+      </div>
+    </>
+  );
 }
