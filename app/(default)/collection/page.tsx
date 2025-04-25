938d37d8da9a003a66153d96dc5cad641c6f8843
Anderson Kouadio <andersonkouadio0118@gmail.com>
2025-04-25 15:10:43 +0000

    mise a jour

--- app/(default)/collection/page.tsx
+++ app/(default)/collection/page.tsx
@@ -4,10 +4,10 @@ import { Watch } from "@/types/watch";
 import Content from "./Content";
 
 export default async function Collection() {
-  // const response = await fetch("https://api.vercel.app/blog");
-  // const watchesTable: Watch[] = await response.json();
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/watch`);
+  const watchesTable: Watch[] = await response.json();
 
-  const watchesTable: Watch[] = watches;
+  // const watchesTable: Watch[] = watches;
 
   return (
     <>
